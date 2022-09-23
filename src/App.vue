9a90b0eb7cd9cc790fd355115c75688411dba8ac
marksmersh <you@example.com>
2022-09-23 07:37:16 +0300

    Add project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  
+</template>
+
+<script setup lang="ts">
+  
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background-color: var(--secondary-bg-color);
+  }
+
+  :root {
+    --bg-color: #FFFFFF;
+    --secondary-bg-color: #F3F2F8;
+    --text-color: #000000;
+    --hint-color: #909090;
+    --link-color: #3196E5;
+    --button_color: #5AC8FB;
+    --button_text_color: #FFFFFF;
+  }
+</style>
